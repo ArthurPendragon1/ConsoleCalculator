c915734c33250154d87544569834c73b8b3ad3fb
ArthurPendragon <kostyan.games.2002@gmail.com>
2024-04-11 16:29:28 +0300

    Project Version 0.7.2

---
+++ ConsoleCalculator/ConsoleCalculator.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int userInput = 0;
+    double firstValue = 0.0, twoValue = 0.0, sum = 0.0;
+    cout << "Enter a two numbers: " << endl;
+    cin >> firstValue >> twoValue;
+
+    cout << "Choose a needed action: " << endl;
+    cin >> userInput;
+
+    switch (userInput)
+    {
+    case 0: sum = firstValue + twoValue;
+        break;
+    case 1: sum = firstValue - twoValue;
+        break;
+    case 2: sum = firstValue * twoValue;
+        break;
+    case 3: sum = firstValue / twoValue;
+        break;
+    }
+
+    cout << "Choosed action is: " << userInput << endl;
+    cout << "Received sum is: " << sum << endl;
+    return 0;
+}
